9c7f5043177cbba5389dc77cde8e7b57348cbf91
vokin23 <kinbiceps@mail.ru>
2024-01-23 06:18:00 +0300

    Django Project vers.1

--- app/app/settings.py
+++ app/app/settings.py
@@ -83,14 +83,11 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'Test',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',  # Предполагается, что база данных находится в корневой папке проекта
     }
 }
+
 
 
 # Password validation
